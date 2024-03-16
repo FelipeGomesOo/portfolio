8ac59f19aefe9fb1de37f377965816bb44bbd5b9
Felipe Gomes <contato@felipe-gomes.com>
2024-03-16 11:44:05 -0300

    chore: move contact component  to root of components folder

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,23 +1,32 @@
-import Image from "next/image"
 import Link from "next/link"
 import {useTranslations} from 'next-intl';
 
 export default function Contact() {
     
-    const t = useTranslations('HomePage.contact');
+    const t = useTranslations('Contact');
     
     return (
     <section id="contatos" className="container my-12 md:my-24"> 
         <h2>{t('title')}</h2>
         <div className="flex flex-wrap ">
             <h3 className="w-full md:w-3/5">                         
-                <a className="block" href="mailto:contato@felipe-gomes.com">contato@felipe-gomes.com</a> 
-                <Image className="inline" width="22" height={22} src="/whatsapp.svg" alt="Whatsapp"/>
-                <span className="telefone">&nbsp;(21)&nbsp;99147-4675</span>
+                contato@felipe-gomes.com 
+                <Link 
+                className=" block" 
+                    target="_blank" 
+                    href="https://api.whatsapp.com/send/?phone=%2B5521991474675&text&type=phone_number&app_absent=0">
+                    <span className="text-primary-light">Whatsapp: </span>{t('whatsapp')} 
+                    
+                </Link>
             </h3>
             <div className="w-full md:w-2/5">
                 <h4>{t('see_also')}</h4> 
                 <div className="space-x-4">
+                    <Link 
+                        target="_blank" 
+                        href="https://github.com/FelipeGomesOo" >
+                        Github
+                    </Link>
                     <Link 
                         target="_blank" 
                         href="https://www.behance.net/FelipeGomesO_o" >
@@ -27,11 +36,6 @@ export default function Contact() {
                         target="_blank" 
                         href="https://www.linkedin.com/in/felipegomesoliveira/" >
                         Linkedin
-                    </Link>
-                    <Link 
-                        target="_blank" 
-                        href="https://github.com/FelipeGomesOo" >
-                        Github
                     </Link>
                 </div>                           
             </div>

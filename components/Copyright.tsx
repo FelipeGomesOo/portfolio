c0b50b46484b8220bbfd1a38b2a0bd9fee021938
Felipe Gomes <contato@felipe-gomes.com>
2024-03-16 10:33:32 -0300

    feat: Change copyright bg

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -1,7 +1,9 @@
 export default function Copyright() {
     const currentYear = new Date().getFullYear();  
     return ( 
-        <section id="copyright" className="container my-6">
-            <small>© {currentYear} – Felipe Gomes</small>
+        <section id="copyright" className="bg-primary text-[#fff]" >
+            <div className="container my-6">
+                <small>© {currentYear} – Felipe Gomes</small>
+            </div>
         </section>
 )}

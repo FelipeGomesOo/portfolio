71a61fc475111e4deab8a1948ffec8ff84200381
Felipe Gomes <contato@felipe-gomes.com>
2024-03-22 09:11:04 -0300

    refactor: improve skillset component logic

--- components/AboutPage/Skillset.tsx
+++ components/AboutPage/Skillset.tsx
@@ -1,12 +1,10 @@
 import { useTranslations,useMessages } from 'next-intl';
 export default function Skillset(){
     const t = useTranslations('AboutPage.skillset');
-    const messages = useMessages();
-
-    const pskills = useTranslations('messages.AboutPage.skillset.programming.skills'); 
-    const pskillsKeys = Object.entries(messages.AboutPage.skillset.programming.skills);
-    const dskills = useTranslations('messages.AboutPage.skillset.design.skills'); 
-    const dskillsKeys = Object.entries(messages.AboutPage.skillset.design.skills);
+    let messages = useMessages().AboutPage;
+    const pskillsKeys = Object.entries(messages.skillset.programming.skills); 
+    const dskillsKeys = Object.entries(messages.skillset.design.skills);
+     
 
     return (
             <section className="my-10 lg:my-16 container grid grid-cols-12 gap-4 lg:gap-6">
@@ -14,9 +12,10 @@ export default function Skillset(){
 
                 <div className="col-span-12 lg:col-span-4">
                     <h3><strong>{t('programming.title')}</strong></h3>
-                    { pskillsKeys.map((skill: string) => {
+                    { pskillsKeys.map((skill: string[], index:number) => {
+                       // console.log(skill ) 
                         return(
-                            <div key={skill} className="w-full " >
+                            <div key={index} className="w-full " >
                                 <h4 className='block'>{skill[0]}</h4>
                                 <h3>{skill[1]}</h3>
                             </div>
@@ -25,9 +24,9 @@ export default function Skillset(){
                 </div> 
                 <div className="col-span-12 lg:col-span-4">
                     <h3><strong>{t('design.title')}</strong></h3>
-                    { dskillsKeys.map((skill:  [string, unknown]) => {
+                    { dskillsKeys.map((skill:  [string, unknown], index:number) => {
                         return(
-                            <div key={skill[0]} className="w-full " >
+                            <div key={index} className="w-full " >
                                 <h4 className='block'>{skill[0]}</h4>
                                 <h3>{skill[1]}</h3>
                             </div>
